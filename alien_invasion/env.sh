8e5182991b42ce945bc17bd6a80cd7bcd051b444
MRNIU <a6813140@hotmail.com>
2018-04-09 23:32:11 +0800

    Chapter12 complete

---
+++ alien_invasion/env.sh
@@ -0,0 +1,2 @@
+brew install hg sdl sdl_image sdl_ttf sdl_mixer portmidi
+pip3 install hg+http://bitbucket.org/pygame/pygame
---
+++ alien_invasion/unenv.sh
@@ -0,0 +1,2 @@
+brew uninstall hg sdl sdl_image sdl_tff sdl_mixer portmidi
+pip3 uninstall hg+http://bitbucket.org/pygame/pygame
