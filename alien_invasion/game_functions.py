bf7c16cc0848d398ec8bd751cc30e60fa9f395d1
MRNIU <a6813140@hotmail.com>
2018-04-10 19:49:46 +0800

    添加注释

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -4,7 +4,7 @@ from bullet import Bullet
 
 
 
-
+# 按键按下
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -17,6 +17,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
 
+
+# 事件检测
 def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -27,6 +29,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
+# 按键松开事件
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -38,7 +41,7 @@ def check_keyup_events(event, ship):
      #   ship.moving_down = False
 
 
-
+# 重绘屏幕
 def update_screen(ai_settings, screen, ship, bullets):
     # 重绘
     screen.fill(ai_settings.bg_color)
@@ -49,13 +52,15 @@ def update_screen(ai_settings, screen, ship, bullets):
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
+
+# 刷新子弹
 def update_bullets(bullets):
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-
+# 开火效果
 def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullet_allowed:
             new_bullet = Bullet(ai_settings, screen, ship)
